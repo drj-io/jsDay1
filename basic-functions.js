43d7a1f4183205c02e2a7133c5ae631f1e9964be
David Jones <david.jones@madwiremedia.com>
2017-04-03 20:15:02 -0600

    finish up for now

---
+++ basic-functions.js
@@ -0,0 +1,8 @@
+function printFoo(howManyFoos){
+    console.log("there are "+ howManyFoos + " foo");
+}
+
+
+for(var i=0; i<10; i++){
+    printFoo(i);
+}
--- functions.js
+++ functions.js
@@ -1,4 +1,39 @@
 
+var people = [
+    {
+        name: "David",
+        favoriteFoods: ["Mexican", "Sushi"],
+        age : 41
+    },
+    {
+        name: "Three",
+        favoriteFoods: ["Mexican", "Home Cooked Meal!"]
+    },
+    {
+        name: "Brian",
+        favoriteFoods: ["Seafood", "Steak"]
+    },
+    {
+        name: "Logan",
+        favoriteFoods: ["Sushi", "Burgers"]
+    },
+    {
+        name: "Molly",
+        favoriteFoods: ["Pasta","Sushi"]
+    },
+    {
+        name: "Spencer",
+        favoriteFoods: ["Curry","Breakfast Burritos"]
+    },
+]
+
+
+// people.forEach(function(person){
+//     printName(person.name)
+// })
+printName("david");
+foo("foooo");
+
 function printName(name){
     console.log("hello, "+name);
 }
@@ -8,15 +43,35 @@ function addNumbers(a,b,c){
     return value;
 }
 
+var addedNumbers = addNumbers(5,5,5);
+
+var placeholder = 0; 
+for (var i =0; i<5; i++){
+    placeholder = addNumbers(5,placeholder,10);    
+}
+console.log(placeholder);
+
 var foo = function(a){
     console.log("ayyy->" + a)
 }
 
+foo("beeee");
+
 var factory = {
-    foo : function(){
-        console.log("foo");
-    },
+    foo : foo,
     bar : function(){
         console.log("bar");
     }
 }
+
+
+var person = {
+    name : "David", 
+    foo : function(){
+        console.log("hello!")
+    }
+}
+
+factory.foo(); 
+
+person.foo();
